4b2ffa2dd3b494a08eea396ec11f7702d56bb5f0
michaelbaez1991 <michael.baez.mendoza1991@gmail.com>
2020-02-25 16:10:09 -0500

    heroku
    Add app heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/Angular9-Spotify'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/Angular9-Spotify/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
